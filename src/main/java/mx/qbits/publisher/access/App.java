db02670edbe002a7c678cd75d35d61cb78bd63c0
arellano-gustavo <arellano.gustavo@gmail.com>
2023-01-07 00:29:31 -0600

    optimizing imports

--- src/main/java/mx/qbits/publisher/access/App.java
+++ src/main/java/mx/qbits/publisher/access/App.java
@@ -2,11 +2,9 @@ package mx.qbits.publisher.access;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
 import lombok.extern.slf4j.Slf4j;
 
 @SpringBootApplication
-//@EnableScheduling
 @Slf4j
 public class App {    
     public static void main(String[] args) {
